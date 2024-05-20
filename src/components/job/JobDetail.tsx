a0b481ea8bc6d7e16b49f9ee007944236df2d765
Void <richmondnkrumah04@gmail.com>
2024-05-20 07:32:12 +0000

    ui modal fixes

--- src/components/job/JobDetail.tsx
+++ src/components/job/JobDetail.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { getRelativeTime } from '@/utils/relativeTimeConvert';
 import { JOB } from '@/types/job';
 import RenderHtml from './RenderHtml';
-const JobDetail = ({ ActiveJob }: {ActiveJob?: JOB} ) => {
+const JobDetail = ({ ActiveJob, isModal }: {ActiveJob?: JOB,isModal: boolean} ) => {
   if (!ActiveJob) return null;
 
   return (
-    <div className="ActiveJob-detail p-4">
+    <div className={`ActiveJob-detail p-4 max-w-[900px] ${isModal && "hidden md:block"}`}>
       <h2 className="text-xl font-bold mb-2">{ActiveJob.role}</h2>
       <p className="mb-2">{ActiveJob.company_name}</p>
       <RenderHtml content={ActiveJob.text} />
