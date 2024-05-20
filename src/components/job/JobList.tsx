589bb3fc953bbd9dac88fb92b0937372a4bad6f1
Void <richmondnkrumah04@gmail.com>
2024-05-20 07:31:51 +0000

    added custom pagination function

--- src/components/job/JobList.tsx
+++ src/components/job/JobList.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import { JOB } from '@/types/job'
-import Job from './Job'
+import React from "react";
+import { JOB } from "@/types/job";
+import Job from "./Job";
 type Props = {
-  jobResults?: JOB[],
-  activeJobHandler: (id: string) => void
-}
+  jobResults?: JOB[];
+  activeJobHandler: (id: string) => void;
+};
+const paginate = (maxList: number, results?: JOB[]) => {
+  if (!results) return [];
+  let finalResults = [];
+  let tempResults = [];
+  let indexPointer = 1;
+  for (let currentJob in results) {
+    tempResults.push(currentJob);
+    if (indexPointer >= maxList) {
+      indexPointer++;
+      finalResults.push(tempResults);
+      tempResults = [];
+    }
+  }
+  return finalResults;
+};
 
-const JobList = ({jobResults,activeJobHandler}: Props) => {
-  console.log(JobList,"this is a jobResults")
+const JobList = ({ jobResults, activeJobHandler }: Props) => {
+  console.log(JobList, "this is a jobResults");
+  const paginatedJobResults = paginate(10, jobResults);
   return (
-    <section className='flex flex-col gap-5'>
-      {jobResults && (
-        jobResults?.map(job => <Job activeJobHandler={activeJobHandler} key={job.id} jobData={job} />)
-      )}
+    <section className="flex flex-col gap-5">
+      {jobResults &&
+        jobResults?.map((job) => (
+          <Job activeJobHandler={activeJobHandler} key={job.id} jobData={job} />
+        ))}
     </section>
-  )
-}
+  );
+};
 
-export default JobList
+export default JobList;
