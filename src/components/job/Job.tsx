3a46f392dca16ac3e7cc7dfd0fc62d374ba96bc5
Void <richmondnkrumah04@gmail.com>
2024-05-20 01:13:57 +0000

    added job related components

---
+++ src/components/job/Job.tsx
@@ -0,0 +1,14 @@
+import { JOB } from '@/types/job'
+import React from 'react'
+
+type Props = {
+  data: JOB
+}
+
+const Job = ({data}: Props) => {
+  return (
+    <div>Job</div>
+  )
+}
+
+export default Job
